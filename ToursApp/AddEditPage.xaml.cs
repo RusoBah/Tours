4e6a301f936498582302432ce31ea2cabab16c45
RusoBah <96246729+RusoBah@users.noreply.github.com>
2023-01-12 19:19:17 +0300

    Add files via upload

---
+++ ToursApp/AddEditPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+
+        // В этот параметр мы будем добовлять экзеплятор выбранного проекта
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+
+            // Если он не пустой
+            if (selectedHotel != null)
+                _currentHotel = selectedHotel;
+
+            DataContext = _currentHotel;
+            ComboCountries.ItemsSource = toursEntities.GetContext().Countries.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка
+            // Переменная для хранения ошибок
+            StringBuilder errors = new StringBuilder();
+
+            // Если название текущего отеля будт пустым
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                // То выведиться строка
+                errors.AppendLine("Укажите название отеля");
+            if (_currentHotel.CountOfStars < 1 || _currentHotel.CountOfStars > 5)
+                errors.AppendLine("Кол-во звёзд - число от 1 до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберети страну");
+
+            // Чтобы узнать возникли ли ошибки нужно:
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            // Если всё хорошо и происходит операция добавления, то будет происходить создание собственного id
+            if (_currentHotel.id == 0)
+                toursEntities.GetContext().Hotels.Add(_currentHotel);
+
+            try
+            {
+                // сохранение изменений
+                toursEntities.GetContext().SaveChanges();
+                MessageBox.Show("Сохранено!");
+                // Возращение на прошлую страницу
+                Maneger.MainFrame.GoBack();
+            }
+               
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+              
+                  
+             
+
+
+               
+           
+        }
+
+        private void ComboCountries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ToursApp/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Отображает первую страницу
+            MainFrame.Navigate(new ToursPage());
+            // Присваивание значение к странице Manager
+            Maneger.MainFrame = MainFrame;
+
+            // Пример импорта
+            ImportTours();
+        }
+
+
+        // Пример импорта
+        private void ImportTours()
+        {
+            // НАДО СОЗДАТЬ ФАЙЛ С ТУРАМИ
+            // В скобках указывается путь для файла
+            var fileData = File.ReadAllLines(@"C:\Users\1ы\Documents\Tours\tours.txt");
+            // для изображения 
+            var Images = Directory.GetFiles(@"C:\Users\1ы\Documents\Tours\img");
+
+            foreach (var line in fileData)
+            {
+                var data = line.Split('\t');
+
+                Tour tempTour;
+                if ((data[4] == "0"))
+                {
+                    tempTour = new Tour
+                {
+                    Name = data[0].Replace("\'", ""),
+                    TicketCount = int.Parse(data[2]),
+                    Price = decimal.Parse(data[3]),
+                    isActual = false
+                };
+                }
+                else
+                {
+                    tempTour = new Tour
+                {
+                    Name = data[0].Replace("\'", ""),
+                    TicketCount = int.Parse(data[2]),
+                    Price = decimal.Parse(data[3]),
+                    isActual = true
+                };
+                }
+
+                foreach (var tourType in data[5].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var currentType = toursEntities.GetContext().Types.ToList().FirstOrDefault(p => p.Name == tourType);
+                    if (currentType != null)
+                        tempTour.Types.Add(currentType);
+                }
+                try
+                {
+                    tempTour.ImagePreview = File.ReadAllBytes(Images.FirstOrDefault(p => p.Contains(tempTour.Name)));
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+                toursEntities.GetContext().Tours.Add(tempTour);
+                toursEntities.GetContext().SaveChanges();
+            }
+
+        }
+
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Maneger.MainFrame.GoBack();
+        }
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
